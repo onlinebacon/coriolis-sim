d345ef764983fb580d22cb06ff01688c8a3eab77
Bacon <bacon.antife@gmail.com>
2022-12-15 10:37:33 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,66 @@
+import Simulation from "./simulation.js";
+
+const inputs = [...document.querySelectorAll('[type="text"]')];
+const runButton = document.querySelector('[type="button"]');
+const textarea = document.querySelector('textarea');
+const freeThread = () => new Promise(done => setTimeout(done, 0));
+
+const distUnits = {
+	'mm': 0.001,
+	'cm': 0.01,
+	'km': 1000,
+	'in': 0.0254,
+	'ft': 0.3048,
+	'mi': 1609.344,
+	'm':  1,
+};
+
+const speedUnits = {
+	'km/h': 1000/3600,
+	'kmph': 1000/3600,
+	'mph': 1609.344/3600,
+	'mps': 1,
+	'm/s': 1,
+	'kph': 1000/3600,
+};
+
+const parseAngle = (str) => Number(str)/180*Math.PI;
+const splitValueUnit = (str) => [...str.trim().match(/^(.*?)\s*([a-z\/]+)?$/)].slice(1);
+const parseDist = (str) => {
+	const [ val, unit ] = splitValueUnit(str.toLowerCase());
+	if (!unit) return Number(val);
+	return val*distUnits[unit];
+};
+const parseSpeed = (str) => {
+	const [ val, unit ] = splitValueUnit(str.toLowerCase());
+	if (!unit) return Number(val);
+	return val*speedUnits[unit];
+};
+
+window.parseDist = parseDist;
+window.parseSpeed = parseSpeed;
+
+const parseMap = {
+	lat: parseAngle,
+	lon: parseAngle,
+	height: parseDist,
+	azm: parseAngle,
+	alt: parseAngle,
+	speed: parseSpeed,
+	radius: parseDist,
+};
+
+runButton.addEventListener('click', async () => {
+	const args = {};
+	inputs.forEach(input => {
+		const name = input.getAttribute('name');
+		const value = parseMap[name]?.(input.value) ?? Number(input.value);
+		args[name] = value;
+	});
+	const sim = Simulation(args);
+	while (!sim.impact) {
+		sim.run(100);
+		textarea.value = JSON.stringify(sim.info(), null, '  ');
+		await freeThread();
+	}
+});
---
+++ simulation.js
@@ -0,0 +1,109 @@
+const { sqrt, sin, cos, asin, acos, PI } = Math;
+const TAU = PI*2;
+
+const toDeg = (rad) => rad*(180/PI);
+const rotateX = ([ x, y, z ], angle) => [ x, y*cos(angle) + z*sin(angle), z*cos(angle) - y*sin(angle) ];
+const rotateY = ([ x, y, z ], angle) => [ x*cos(angle) - z*sin(angle), y, z*cos(angle) + x*sin(angle) ];
+const rotateZ = ([ x, y, z ], angle) => [ x*cos(angle) + y*sin(angle), y*cos(angle) - x*sin(angle), z ];
+const scale = ([ x, y, z ], scale) => [ x*scale, y*scale, z*scale ];
+const add = ([ ax, ay, az ], [ bx, by, bz ]) => [ ax + bx, ay + by, az + bz ];
+
+const getCoords = ([ x, y, z ]) => {
+	const lat = asin(y/sqrt(x*x + y*y + z*z));
+	const lon = acos(z/sqrt(z*z + x*x))*(x>=0?1:-1);
+	return [ lat, lon ];
+};
+
+const getPosition = (lat, lon, radius, height) => {
+	let pos = [ 0, 0, radius + height ];
+	pos = rotateX(pos, lat);
+	pos = rotateY(pos, -lon);
+	return pos;
+};
+
+const getLocalSpeed = (lat, lon, azm, alt, magnitude) => {
+	let dir = [ 0, 1, 0 ];
+	dir = rotateX(dir, -alt);
+	dir = rotateZ(dir, azm);
+	dir = rotateX(dir, lat);
+	dir = rotateY(dir, -lon);
+	return scale(dir, magnitude);
+};
+
+const getTangentialSpeed = (lat, lon, radius, height, period) => {
+	const circ = TAU*(radius + height)*cos(lat);
+	const magnitude = circ/period;
+	return rotateY([ magnitude, 0, 0 ], -lon);
+};
+
+const Simulation = ({
+	lat,
+	lon,
+	height,
+	azm,
+	alt,
+	speed,
+	radius,
+	rotationPeriod,
+	deltaTime,
+	gSurfaceAcc,
+}) => {
+	let [ px, py, pz ] = getPosition(lat, lon, radius, height);
+	let [ vx, vy, vz ] = add(
+		getLocalSpeed(lat, lon, azm, alt, speed),
+		getTangentialSpeed(lat, lon, radius, height, rotationPeriod),
+	);
+	const G = gSurfaceAcc*radius*radius;
+	return {
+		px, py, pz,
+		vx, vy, vz,
+		dt: deltaTime,
+		time: 0,
+		radius,
+		rotationPeriod,
+		G,
+		itCount: 0,
+		impact: false,
+		run: function(nIt) {
+			let { px, py, pz, vx, vy, vz, radius, dt, time, G, itCount } = this;
+			for (let i=0; i<nIt; ++i) {
+				const t = px*px + py*py + pz*pz;
+				const d = sqrt(t);
+				if (d <= radius) break;
+				const dir_x = -px/d;
+				const dir_y = -py/d;
+				const dir_z = -pz/d;
+				const mag = G/t;
+				vx += mag*dir_x*dt;
+				vy += mag*dir_y*dt;
+				vz += mag*dir_z*dt;
+				px += vx*dt;
+				py += vy*dt;
+				pz += vz*dt;
+				time += dt;
+				++ itCount;
+			}
+			if (this.impact === false) {
+				this.impact = sqrt(px*px + py*py + pz*pz) <= radius;
+			}
+			this.px = px;
+			this.py = py;
+			this.pz = pz;
+			this.vx = vx;
+			this.vy = vy;
+			this.vz = vz;
+			this.time = time;
+			this.itCount = itCount;
+			return this;
+		},
+		info: function() {
+			const { px, py, pz, time, rotationPeriod } = this;
+			let rotated = TAU*(time/rotationPeriod);
+			let pos = rotateY([ px, py, pz ], rotated);
+			let coord = getCoords(pos).map(toDeg);
+			return { impact: this.impact, time: time, coord };
+		}
+	};
+};
+
+export default Simulation;
